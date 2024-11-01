dfcf0de5d13ad60fdab0a8ad9644b4b5e69dfdcb
ktp <misterkantapat@gmail.com>
2024-11-01 21:00:17 +0700

    change dir file path on ButtonMouseClicked

--- src/Course/ObjReader.java
+++ src/Course/ObjReader.java
@@ -17,28 +17,28 @@ public class  ObjReader {
         return c;
     }
 
-    public static void main(String[] args) {
-//        CreateObjCourse c = new CreateObjCourse("src/CSVfileTest/Course_CPE.csv");
-        Course cpe = ObjReader.readObj("src/CourseObjFile/Course_CPE.ser");
-        // Using this line to run test -->
-        for(String code : cpe.getAllsubCode()) {
-            System.out.println("Name: " + cpe.getIdMap().get(code).getName());
-            System.out.println("id: " + cpe.getIdMap().get(code).getId());
-            System.out.print("Require-> ");
-            if(cpe.getIdMap().get(code).getRequire().isEmpty())
-                System.out.print("null");
-            for(Subject s : cpe.getIdMap().get(code).getRequire()) {
-                System.out.print(s.getId()+" ");
-            }
-            System.out.print("\nNext-> ");
-            if(cpe.getIdMap().get(code).getNext().isEmpty())
-                System.out.print("null");
-            for(Subject s : cpe.getIdMap().get(code).getNext()) {
-                System.out.print(s.getId()+" ");
-            }
-            System.out.println("\n");
-        }
-        // run test -->
-
-    }
+//    public static void main(String[] args) {
+////        CreateObjCourse c = new CreateObjCourse("src/CSVfileTest/Course_CPE.csv");
+//        Course cpe = ObjReader.readObj("src/CourseObjFile/Course_CPE.ser");
+//        // Using this line to run test -->
+//        for(String code : cpe.getAllsubCode()) {
+//            System.out.println("Name: " + cpe.getIdMap().get(code).getName());
+//            System.out.println("id: " + cpe.getIdMap().get(code).getId());
+//            System.out.print("Require-> ");
+//            if(cpe.getIdMap().get(code).getRequire().isEmpty())
+//                System.out.print("null");
+//            for(Subject s : cpe.getIdMap().get(code).getRequire()) {
+//                System.out.print(s.getId()+" ");
+//            }
+//            System.out.print("\nNext-> ");
+//            if(cpe.getIdMap().get(code).getNext().isEmpty())
+//                System.out.print("null");
+//            for(Subject s : cpe.getIdMap().get(code).getNext()) {
+//                System.out.print(s.getId()+" ");
+//            }
+//            System.out.println("\n");
+//        }
+//        // run test -->
+//
+//    }
 }
