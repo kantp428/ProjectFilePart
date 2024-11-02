b3635c226b714ed45d09d6cad66b46abaa5bad76
Deadchicken07 <deadchicken07@gmail.com>
2024-11-02 15:33:58 +0700

    Can Login and Register2

--- src/Program/Program.java
+++ src/Program/Program.java
@@ -13,13 +13,13 @@ public class Program {
 //        User user1 = new Lecturer("a123","Tanongtuy","Kongkonkoy","tek027454184",a);
 //        User user2 = new Student("b6620503578","Supakorn","Leelatanangkul","tek027454184",a);
 //        AllUser.writeUserObjFile(a);
-        AllUser b = AllUser.readUserObjFile();
+//        AllUser b = AllUser.readUserObjFile();
 //        AllUser b = AllUser.readUserObjFile();
 //        AllUser.writeUserObjFile(a);
 //        AllUser b = AllUser.readUserObjFile();
-        for(String i : b.getAllkey()) {
-            System.out.println(i);
-        }
+//        for(String i : b.getAllkey()) {
+//            System.out.println(i);
+//        }
 //        System.out.println(b.getUserMap().get("b6620503578").getFullName());
           new Login().setVisible(true);
     }
